1f32de66caadc01d407155538530fd60a372d897
masa-mk43 <92095767+masa-mk43@users.noreply.github.com>
2025-11-02 17:40:18 +0900

    Set DEBUG to False and restrict ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['masa1981.pythonanywhere.com']
 
 
 # Application definition
