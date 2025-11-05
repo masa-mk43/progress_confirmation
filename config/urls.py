f1e0c6a7118db75dffcfda31c83621709f8869df
masa-mk43 <masa1981music@gmail.com>
2025-11-05 19:50:37 +0900

    add_lp_page

--- config/urls.py
+++ config/urls.py
@@ -19,10 +19,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('progress_confirmation.urls')),
+    path('', TemplateView.as_view(template_name="lp.html"), name='landing'),
+    path('app/', include('progress_confirmation.urls')),
 ]
 
 if settings.DEBUG:
